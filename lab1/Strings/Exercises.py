345db58ac1bc7f352b2d8951487c7014668a2944
Ravil180904 <ravil.moldabaev09@gmail.com>
2024-02-08 23:13:20 +0600

    Lab works

---
+++ lab1/Strings/Exercises.py
@@ -0,0 +1,12 @@
+#Exercise
+
+#1. Use the len function to print the length of the string.
+x = "Hello World"
+print(len(x), "\n")
+
+#2 Get the first character of the string txt.
+txt = "Hello World"
+x = txt[0]
+print(x, "\n")
+
+#3 Get the characters from index 2 to index 4 (llo)
