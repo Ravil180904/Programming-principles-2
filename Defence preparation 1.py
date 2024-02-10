b2d46e9f5b7a4d576f894a5050f00ba2b1350dac
Ravil180904 <ravil.moldabaev09@gmail.com>
2024-02-10 11:29:54 +0600

    Lab works

--- Defence preparation 1.py
+++ Defence preparation 1.py
@@ -1,15 +1,30 @@
-#Write a recursive function fibonacci that returns the nth Fibonacci number.
+#1. Write a function called calculate_factorial that takes a positive integer as a parameter and returns its factorial (use recursion)
 
-def Fibonacci(n):
-    if n<= 0:
-        print("Error")
-    elif n == 1:
-        return 0
-    elif n == 2:
+def factorial(n):
+    if n == 0:
         return 1
     else:
-        return Fibonacci(n-1)+Fibonacci(n-2)
-    
-print(Fibonacci(n))
+        return n * factorial(n-1)
+n = int(input())
+print(factorial(5))
 
-
+#2. Write a recursive function fibonacci that returns the nth Fibonacci number.
+#q = input()
+def fibonacci(n):
+    a = 1
+    b = 2
+    if n<0:
+        print("Error")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+            #q += 1
+        return c
+n = int(input())
+print(fibonacci(n))
