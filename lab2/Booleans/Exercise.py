981e4e03a23e5a50f2c132f7d28b93a535db6eef
Ravil180904 <ravil.moldabaev09@gmail.com>
2024-02-10 11:03:25 +0600

    Lab works

--- lab2/Booleans/Exercise.py
+++ lab2/Booleans/Exercise.py
@@ -1,14 +1,14 @@
-#Exercise 1
+#Exercise 1 The statement below would print a Boolean value, which one?
 print(10 > 9) #True
 
-#Exercise 2
+#Exercise 2 The statement below would print a Boolean value, which one?
 print(10 == 9) #False
 
-#Exercise 3
+#Exercise 3 The statement below would print a Boolean value, which one?
 print(10 < 9) #False
 
-#Exercise 4
+#Exercise 4 The statement below would print a Boolean value, which one?
 print(bool("abc")) #True
 
-#Exercise 5
+#Exercise 5 The statement below would print a Boolean value, which one?
 print(bool(0)) #False
