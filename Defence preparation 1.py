4bf8aae83f64308f63df70ea1a13f1eccb85b668
Ravil180904 <ravil.moldabaev09@gmail.com>
2024-02-10 11:45:30 +0600

    Defence 1

--- Defence preparation 1.py
+++ Defence preparation 1.py
@@ -1,15 +1,13 @@
 #1. Write a function called calculate_factorial that takes a positive integer as a parameter and returns its factorial (use recursion)
-
-def factorial(n):
+def calculate_factorial(n):
     if n == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * calculate_factorial(n-1)
 n = int(input())
-print(factorial(5))
+print(calculate_factorial(n))
 
 #2. Write a recursive function fibonacci that returns the nth Fibonacci number.
-#q = input()
 def fibonacci(n):
     a = 1
     b = 2
@@ -24,7 +22,16 @@ def fibonacci(n):
             c = a + b
             a = b
             b = c
-            #q += 1
         return c
 n = int(input())
 print(fibonacci(n))
+
+
+#3. Write a function called is_even that takes an integer as a parameter and returns True if it's even and False otherwise.
+n = int(input())
+def is_even(n):
+    if(n % 2 == 0):
+        return True
+    else:
+        return False
+print(is_even(n))
