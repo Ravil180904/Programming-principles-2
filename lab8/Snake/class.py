3ba7c804c6e370e3a985c4f05c2c72204404d828
Ravil180904 <ravil.moldabaev09@gmail.com>
2024-04-13 12:49:26 +0500

    Lab works

---
+++ lab8/Snake/class.py
@@ -0,0 +1,125 @@
+import pygame
+import sys
+import random
+
+pygame.init()
+
+# color palette
+colorBLACK = (0, 0, 0)
+colorGRAY = (200, 200, 200)
+colorWHITE = (255, 255, 255)
+colorRED = (255, 0, 0)
+colorGREEN = (0, 255, 0)
+colorBLUE = (0, 0, 255)
+colorYELLOW = (255, 255, 0)
+
+WIDTH = 600
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+FPS = 5
+clock = pygame.time.Clock()
+
+CELL = 30
+
+def draw_grid():
+    for i in range(HEIGHT // CELL):
+        for j in range(WIDTH // CELL):
+            pygame.draw.rect(screen, colorGRAY, (i * CELL, j * CELL, CELL, CELL), 3)
+
+def draw_grid_chess():
+    colors = [colorWHITE, colorGRAY]
+
+    for i in range(HEIGHT // CELL):
+        for j in range(WIDTH // CELL):
+            pygame.draw.rect(screen, colors[(i + j) % 2], (i * CELL, j * CELL, CELL, CELL))
+
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+class Snake:
+    def __init__(self):
+        self.body = [Point(10, 11), Point(10, 12), Point(10, 13)]
+        self.dx = 1
+        self.dy = 0
+
+    def move(self):
+        for i in range(len(self.body) - 1, 0, -1):
+            self.body[i].x = self.body[i - 1].x
+            self.body[i].y = self.body[i - 1].y
+        
+        self.body[0].x += self.dx
+        self.body[0].y += self.dy
+
+    def draw(self):
+        head = self.body[0]
+        pygame.draw.rect(screen, colorRED, (head.x * CELL, head.y * CELL, CELL, CELL))
+        for segment in self.body[1:]:
+            pygame.draw.rect(screen, colorYELLOW, (segment.x * CELL, segment.y * CELL, CELL, CELL))
+
+    def check_collision(self, food):
+        head = self.body[0]
+        if head.x == food.pos.x and head.y == food.pos.y:
+            self.body.append(Point(head.x, head.y))
+            return True
+        return False
+
+class Food:
+    def __init__(self, snake_body):
+        self.snake_body = snake_body #Point(15, 15)
+        self.random_position()
+        def random_position(self):
+            while True:
+                x = random.randint(0, (WIDTH // CELL) - 1)
+                y = random.randint(0, (HEIGHT // CELL) - 1)
+            # Check if the position is not on a wall or snake's body
+                if (x, y) not in [(p.x, p.y) for p in snake_body]:
+                    return Point(x, y)
+        def draw(self):
+            pygame.draw.rect(screen, colorGREEN, (self.pos.x * CELL, self.pos.y * CELL, CELL, CELL))
+     
+    def move(self):
+        pass
+
+    def draw(self):
+        pygame.draw.rect(screen, colorGREEN, (self.pos.x * CELL, self.pos.y * CELL, CELL, CELL))
+
+done = False
+
+snake = Snake()
+food = Food(snake.body)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake.dx = 1
+                snake.dy = 0
+            if event.key == pygame.K_LEFT:
+                snake.dx = -1
+                snake.dy = 0
+            if event.key == pygame.K_UP:
+                snake.dx = 0
+                snake.dy = -1
+            if event.key == pygame.K_DOWN:
+                snake.dx = 0
+                snake.dy = 1
+
+    draw_grid_chess()
+
+    snake.move()
+    snake.draw()
+
+    food.draw()
+
+    if snake.check_collision(food):
+        print("Got food!")
+    
+    pygame.display.flip()
+    clock.tick(FPS)
